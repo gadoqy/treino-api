2a285b7f290ac19f19e8981026c85a0e0380a209
Light.sp <gadoqy@gmail.com>
2024-08-09 16:33:23 -0300

    Test 1

--- leituraLivro.js
+++ leituraLivro.js
@@ -4,8 +4,7 @@ console.clear();
 
 const servidor = express();
 const PORT = 157;
-
-servidor.use(express.json());  
+servidor.use(express.json());
 
 servidor.post('/treino/leituraLivro', (req, res) => {
     const livro = req.body.livro;
@@ -20,6 +19,26 @@ servidor.post('/treino/leituraLivro', (req, res) => {
         tempoTotalHoras: tempoTotalHoras.toFixed(2)
     });
 });
+
+servidor.post('/verificarAssistir', (req, res) => {
+    const { idadePessoa1, idadePessoa2, Classificacao } = req.body;
+
+    if (idadePessoa1 === undefined || idadePessoa2 === undefined || Classificacao === undefined) {
+        return res.status(400).json({ error: "Você deve fornecer idadePessoa1, idadePessoa2 e Classificacao." });
+    }
+
+    const classificacao = parseInt(Classificacao, 10);
+
+    if (![0, 12, 14, 16, 18].includes(classificacao)) {
+        return res.status(400).json({ error: "Classificação inválida. Deve ser um dos valores: 0, 12, 14, 16, 18." });
+    }
+
+    const podeAssistir = idadePessoa1 >= classificacao && idadePessoa2 >= classificacao;
+
+
+    res.json({ podeAssistir });
+});
+
 
 servidor.listen(PORT, () => {
     console.log(`API subiu na porta ${PORT}`);
